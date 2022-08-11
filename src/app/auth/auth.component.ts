4ebe5792923bbc5762d1cc414c50d7f33f3d1fd5
Noam Honig <noam.honig@gmail.com>
2022-08-11 17:53:51 +0300

    demo first draft

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -4,25 +4,32 @@ import { UserInfo } from 'remult';
 
 @Component({
   selector: 'app-auth',
-  templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.css']
+  templateUrl: './auth.component.html'
 })
 export class AuthComponent implements OnInit {
 
-  constructor(private http: HttpClient) {
-
-  }
+  constructor(private http: HttpClient) { }
   signInUsername = '';
   currentUser?: UserInfo;
+
   signIn() {
-
+    this.http.post<UserInfo>('/api/signIn',
+      {
+        username: this.signInUsername
+      }).subscribe({
+        next: user => this.currentUser = user,
+        error: error => alert(error.error)
+      });
   }
+
   signOut() {
-
+    this.http.post('/api/signOut', {})
+      .subscribe(() => this.currentUser = undefined);
   }
 
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.http.get<UserInfo>('/api/currentUser')
+      .subscribe(user => this.currentUser = user)
   }
 
 }

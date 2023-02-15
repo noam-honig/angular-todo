54c4dda7dc75c4681c4ed074417eb48398e9b99a
Noam Honig <noam.honig@gmail.com>
2023-02-15 14:35:36 +0200

    some adjustments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { NgModule, NgZone } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { HttpClientModule, HttpClient } from '@angular/common/http'
+import { FormsModule } from '@angular/forms'
 
-import { AppComponent } from './app.component';
-import { TodoComponent } from './todo/todo.component';
-import { AuthComponent } from './auth/auth.component';
-
+import { AppComponent } from './app.component'
+import { TodoComponent } from './todo/todo.component'
+import { AuthComponent } from './auth/auth.component'
+import { remult } from 'remult'
 
 @NgModule({
   declarations: [
@@ -22,4 +22,10 @@ import { AuthComponent } from './auth/auth.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(zone: NgZone, http: HttpClient) {
+    remult.apiClient.httpClient = http
+    remult.apiClient.wrapMessageHandling = (handler) =>
+      zone.run(() => handler())
+  }
+}

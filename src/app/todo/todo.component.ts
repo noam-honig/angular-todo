2471f44be12874c775f156f77879f7ba04d91b85
Noam Honig <noam.honig@gmail.com>
2023-09-18 08:02:58 +0300

    aligned css

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -6,27 +6,26 @@ import { Task } from './task'
   templateUrl: './todo.component.html'
 })
 export class TodoComponent implements OnInit, OnDestroy {
-  
   tasks: Task[] = [
-    { id: 1, title: 'Setup', completed: true },
-    { id: 2, title: 'Entities', completed: false },
-    { id: 3, title: 'Paging, Sorting and Filtering', completed: false },
-    { id: 4, title: 'CRUD Operations', completed: false },
-    { id: 5, title: 'Live Query', completed: false },
-    { id: 6, title: 'Validation', completed: false },
-    { id: 7, title: 'Backend methods', completed: false },
-    { id: 8, title: 'Database', completed: false },
-    { id: 9, title: 'Authentication and Authorization', completed: false },
-    { id: 10, title: 'Deployment', completed: false }
+    { id: '1', title: 'Setup', completed: true },
+    { id: '2', title: 'Entities', completed: false },
+    { id: '3', title: 'Paging, Sorting and Filtering', completed: false },
+    { id: '4', title: 'CRUD Operations', completed: false },
+    { id: '5', title: 'Live Query', completed: false },
+    { id: '6', title: 'Validation', completed: false },
+    { id: '7', title: 'Updating multiple tasks', completed: false },
+    { id: '8', title: 'Database', completed: false },
+    { id: '9', title: 'Authentication and Authorization', completed: false },
+    { id: '10', title: 'Deployment', completed: false }
   ]
   ngOnInit() {}
   ngOnDestroy() {}
-  
+
   newTaskTitle = ''
   async addTask() {
     try {
       this.tasks.push({
-        id: this.tasks.length,
+        id: (this.tasks.length + 1).toString(),
         title: this.newTaskTitle,
         completed: false
       })
